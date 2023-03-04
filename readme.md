996b5ebfdebca2b1e0412a6f068ad9d76acadecc
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-03-04 23:04:50 +0530

    delivery screen

---
+++ readme.md
@@ -0,0 +1,25 @@
+
+# Deliveroo 2.0
+
+Deliveroo clone built with react-native and sanity.io
+
+
+## Tech Stack
+
+**Client:** React, react-native,Redux, TailwindCSS
+
+**Server:** Sanity.io
+
+
+## Screenshots
+
+![App Screenshot](https://user-images.githubusercontent.com/75252077/222920300-9b62d740-93ee-4caf-ad16-8cccde6e05eb.png)
+
+![App Screenshot](https://user-images.githubusercontent.com/75252077/222920315-60c411fa-1b15-4e20-83d2-20250f92b6f2.png)
+
+![App Screenshot](https://user-images.githubusercontent.com/75252077/222920351-f9808831-fbd4-4e4e-a3d7-040c89154e7f.png)
+
+![App Screenshot](https://user-images.githubusercontent.com/75252077/222920357-18ed3be4-d213-445c-a30e-68a949256b62.png)
+
+
+
--- src/screens/DeliveryScreen.js
+++ src/screens/DeliveryScreen.js
@@ -1,24 +1,65 @@
-import { View, Text,SafeAreaView } from 'react-native'
-import React from 'react'
-import { XIcon } from "react-native-heroicons/solid";
+import { View, Text, SafeAreaView, Image,TouchableOpacity } from "react-native";
+import React from "react";
+import { XMarkIcon } from "react-native-heroicons/solid";
 import * as Progress from "react-native-progress";
 import { useNavigation } from "@react-navigation/native";
 import { selectRestaurant } from "../features/restaurantSlice";
 import { useSelector } from "react-redux";
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from "react-native-animatable";
 
 const DeliveryScreen = () => {
-
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
 
   return (
     <View className="bg-[#00CCBB] flex-1">
-    <SafeAreaView>
-      <Text>DeliveryScreen</Text>
-    </SafeAreaView>
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <XMarkIcon color={"white"} size={30} />
+          </TouchableOpacity>
+          <Text className="text-white text-lg">Order Help!</Text>
+        </View>
+        <View className="bg-white mx-5 my-2 rounded-md p-6 shadow-md z-50">
+          <View className="flex-row justify-between ">
+            <View>
+              <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+              <Text className="font-bold text-4xl">40-45 mins</Text>
+            </View>
+            <Image
+              source={{
+                uri: "https://links.papareact.com/fls",
+              }}
+              className="h-20 w-20"
+            />
+          </View>
+          <Progress.Bar size={30} color="#00CCBB" indeterminate={true} />
+          <Text className="mt-3 text-gray-500">
+            Your Order at {restaurant.title} is being prepared
+          </Text>
+        </View>
+      </SafeAreaView>
+      <Animatable.Image
+        source={require("../../assets/deliveryguy.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-full"
+      />
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28 pb-10">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-12 w-12 bg-gray-300 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Ghost Rider</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+        <Text className="text-[#00CCBB] font-semibold text-lg mr-5">Call</Text>
+      </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default DeliveryScreen
+export default DeliveryScreen;
